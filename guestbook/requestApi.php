9ea3f00cfe8dcaf2d007fff3fa32b9fbd443a4b0
exari <82609830+exaricode@users.noreply.github.com>
2022-11-14 09:18:35 +0100

    name change

--- guestbook/requestApi.php
+++ guestbook/requestApi.php
@@ -1,14 +1,14 @@
-<?php
+<?php declare(strict_types=1);
 
 function delete(string $data) : string {
     $path = '../berichten/berichten.json';
     $file = file_get_contents($path);
-    $test = (array) json_decode($file, null, 512, JSON_OBJECT_AS_ARRAY);
+    $message = (array) json_decode($file, null, 512, JSON_OBJECT_AS_ARRAY);
 
-    for ($i = 0; $i < count($test); $i++) {
-        if ($test[$i]['message'] == $data) {
-            array_splice($test, $i, 1);
-            file_put_contents($path, json_encode($test, JSON_PRETTY_PRINT));
+    for ($i = 0; $i < count($message); $i++) {
+        if ($message[$i]['message'] == $data) {
+            array_splice($message, $i, 1);
+            file_put_contents($path, json_encode($message, JSON_PRETTY_PRINT));
             return 'succes';
         }
     }
