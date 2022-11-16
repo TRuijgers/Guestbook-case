534d68b7b02978455f8bec5b9a0fdbf82dc6af73
TRuijgers <26258112+TRuijgers@users.noreply.github.com>
2022-11-16 11:21:23 +0100

    index links fix

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once('./guestbook/guestbook.php');
     <title>guestbook</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/styles.css">
+    <link href="./css/styles.css" rel="stylesheet">
     <script src="./scripts/main.js" defer></script>
 </head>
 
@@ -23,15 +23,15 @@ require_once('./guestbook/guestbook.php');
                 <p>Laat hier je bericht achter.</p>
                 <label for="firstName" >First Name</label>                
                 <span class="error-empty"><?php if (isset($firstNameError)) echo $firstNameError;?></span>
-                <input type="text" name="firstName" value="<?php if (isset($_POST['firstName'])) echo $_POST['firstName']; ?>">
+                <input type="text" name="firstName" value="<?php if (isset($_POST['firstName'])) echo $_POST['firstName']; ?>" required>
 
                 <label for="lastName" >Last Name</label>                 
                 <span class="error-empty"><?php if (isset($lastNameError)) echo $lastNameError;?></span>
-                <input type="text" name="lastName" value="<?php if (isset($_POST['lastName'])) echo $_POST['lastName']; ?>">
+                <input type="text" name="lastName" value="<?php if (isset($_POST['lastName'])) echo $_POST['lastName']; ?>" required>
 
                 <label for="message" >Message</label>                
                 <span class="error-empty"><?php if (isset($messageError)) echo $messageError;?></span>
-                <textarea name="message"><?php if (isset($_POST['message'])) echo $_POST['message'];?></textarea>
+                <textarea name="message" required><?php if (isset($_POST['message'])) echo $_POST['message'];?></textarea>
 
                 <input type="submit" name="submit" value="Submit">
             </form>
@@ -41,24 +41,24 @@ require_once('./guestbook/guestbook.php');
             <section id="pagination" class="pagination bg-light d-flex justify-content-center" >
                 <?php
                     $limit = 10;
-                    $start = $_GET['page'] && $_GET['page'] > 1 ? 
+                    $start = isset($_GET['page']) && $_GET['page'] > 1 ? 
                         $_GET['page'] * $limit - $limit : 1;
                     $page = $_GET['page'] ?? 1;
 
                     $totalPages = ceil(count(Guestbook::getMessages()) / $limit);
                     if ($page > 1) {
-                        echo "<button class=\"page-item\" 
-                            id=\"previous\">&laquo;</button>";
+                        echo "<li class=\"page-item\" 
+                            id=\"previous\">&laquo;</li>";
                     }
                     for($i = 0; $i < $totalPages; $i++) {
                         $num = $i + 1;
-                        echo "<button class=\"page-item\">
-                            <a class=\"page-link\" href=\"/?page=${num}\">
-                             ${num}</a></button>";
+                        echo "<li class=\"page-item\">
+                            <a class=\"page-link\" href=\"?page=${num}\">
+                             ${num}</a></li>";
                     }
                     if ($page < $totalPages){
-                        echo "<button class=\"page-item\" 
-                            id=\"next\">&raquo;</button>";
+                        echo "<li class=\"page-item\" 
+                            id=\"next\">&raquo;</li>";
                     }
                 ?>
             </section>
@@ -86,16 +86,14 @@ window.addEventListener('load', () => {
     if (nextBtn) {
         nextBtn.addEventListener('click', () => {
             let page = <?php echo $page; ?> + 1;
-            window.location.href = window.location.protocol + "//" + 
-            window.location.hostname + `/?page=${page}`;
+            window.location.href = `?page=${page}`;
         });
     }
 
     if (prevBtn) {
         prevBtn.addEventListener('click', () => {
             let page = <?php echo $page; ?> - 1 ;
-            window.location.href = window.location.protocol + "//" + 
-            window.location.hostname + `/?page=${page}`;
+            window.location.href = `?page=${page}`;
         });
     }
 });
